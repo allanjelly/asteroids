6e2f13de8f51f75ec2de5273994054f0ba846efa
allanjelly <arekklis@gmail.com>
2025-07-17 16:37:18 +0200

    player rotating

--- main.py
+++ main.py
@@ -20,10 +20,13 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        dt = clock.tick(60)/1000
+
+        player.update(dt)
         player.draw(screen)
 
         pygame.display.flip()
-        dt = clock.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
